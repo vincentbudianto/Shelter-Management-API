d0400a081c61cb992d62cfd282adef3f94e8b15b
ChristzenLeonardy <christzenleonardy@gmail.com>
2020-03-10 09:55:58 +0700

    Add CORS

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    cors = require('cors');
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
