b99ef62cecd42acec910104932d308d223dfb3fd
vincentbudianto <vincent.budianto@gmail.com>
2020-04-26 20:30:51 +0700

    commit for deploy to heroku

--- conn.js
+++ conn.js
@@ -1,18 +1,25 @@
 var mysql = require('mysql');
 
 if (process.env.JAWSDB_URL) {
-  con = mysql.createConnection(process.env.JAWSDB_URL);
+  var con = mysql.createConnection(process.env.JAWSDB_URL);
+  console.log("Heroku");
 } else {
-  con = mysql.createConnection({
+  var con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
     database: "shelter_management"
   });
+  console.log("Local");
 };
 
 con.connect(function (err) {
-    if (err) throw err;
+  if (err) {
+    console.log("Error");
+    throw err;
+  } else {
+    console.log("Connected");
+  }
 });
 
 module.exports = con;
